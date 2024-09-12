48fbc918ee5bfeb149f829f604d5ec072aef50c0
slebetman <slebetman@gmail.com>
2024-09-12 20:17:58 +0800

    Add simple UI

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+	});
+	
+	win.loadFile('index.html');
+});
+
+app.on('window-all-closed', () => {
+	app.quit();
+});
