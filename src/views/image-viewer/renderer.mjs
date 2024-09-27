91635b507d4743c4b6b4f97d68f5aab4ba461e37
slebetman <slebetman@gmail.com>
2024-09-27 15:32:46 +0800

    Minor update

--- src/views/image-viewer/renderer.mjs
+++ src/views/image-viewer/renderer.mjs
@@ -1,14 +1,15 @@
 import { imgViewer } from '../components/img-viewer.mjs';
-import { make, render } from '../lib/dom-utils.mjs'
-import { safePath } from '../lib/safe-path.mjs';
+import { render } from '../lib/dom-utils.mjs'
 
 async function main () {
 	api.imgListener((stat) => {
-		// image: imgPath,
-		// width: stat.width,
-		// height: stat.height,
-		// type: stat.format,
-		// size: readable(stat.size),
+		// stat: {
+		//   image: imgPath,
+		//   width,
+		//   height,
+		//   type: stat.format,
+		//   size: readable(stat.size),
+		// }
 
 		const img = imgViewer({imgPath: stat.image});
 
