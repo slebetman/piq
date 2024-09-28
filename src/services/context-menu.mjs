93efb9654eccaec427ee1018ae9fe47662cf71b8
slebetman <slebetman@gmail.com>
2024-09-28 13:10:46 +0800

    Add copy commands to context menu

--- src/services/context-menu.mjs
+++ src/services/context-menu.mjs
@@ -1,5 +1,7 @@
-import { ipcMain, Menu } from "electron";
-import open from "open";
+import { clipboard, ipcMain, Menu, nativeImage } from 'electron';
+import open from 'open';
+import path from 'path';
+import sharp from 'sharp';
 
 export async function init () {
 	ipcMain.handle('context-menu-img', async (e, filePath) => {
@@ -15,7 +17,28 @@ export async function init () {
 				click: () => {
 					open(filePath, {app: {name: 'gimp'}});
 				}
-			}
+			},
+			{ type: 'separator'},
+			{
+				label: 'Copy file path',
+				click: () => {
+					clipboard.writeText(filePath);
+				}
+			},
+			{
+				label: 'Copy directory path',
+				click: () => {
+					clipboard.writeText(path.dirname(filePath));
+				}
+			},
+			{
+				label: 'Copy image',
+				click: async () => {
+					const imgBuffer = await sharp(filePath).png().toBuffer();
+					const img = nativeImage.createFromBuffer(imgBuffer);
+					clipboard.writeImage(img);
+				}
+			},
 		];
 
 		const menu = Menu.buildFromTemplate(template);
